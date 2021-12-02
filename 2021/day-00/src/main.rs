554bfb0b07d5cc016660ab97bf37ceea68b485ee
Tobias Kohlmaier <tobias.kohlmaier@bearingpoint.com>
2021-12-02 10:15:50 +0100

    Improve template project

--- 2021/day-00/src/main.rs
+++ 2021/day-00/src/main.rs
@@ -1,13 +1,21 @@
-use std::io::Cursor;
+macro_rules! debug_log {
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        {
+            println!($($arg)*);
+        }
+    };
+}
 
 use tokio::io::BufReader;
 
 #[tokio::main]
 async fn main() {
-    use tokio::io::AsyncBufRead;
+    use tokio::io::AsyncBufReadExt;
     let reader = BufReader::new(tokio::io::stdin());
-    let lines = reader.lines();
-    while let Some(line) = lines.next_line() {
-
+    let mut lines = reader.lines();
+    while let Some(line) = lines.next_line().await.expect("IO error") {
+        // Do something with the line input
+        debug_log!("{}", line);
     }
 }
